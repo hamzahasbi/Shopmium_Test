7ad13f82047183538b7c31eb5ee0a69230dae2b5
hamzahasbi <hamza.hasbi@gmail.com>
2017-11-29 20:22:18 +0000

    En cours de dev

--- app/controllers/positions_controller_controller.rb
+++ app/controllers/positions_controller_controller.rb
@@ -3,15 +3,48 @@ class PositionsControllerController < ApplicationController
   def index
     @list = Shop.all
   end
-
   def new
+  end
+  def add
+    @new_Shop = Shop.new
+    @new_Shop.chain = params[:chain] if(params[:chain] != nil and params[:chain] != "")
+    @new_Shop.name = params[:name] if(params[:name] != nil and params[:name] != "")
+    @new_Shop.zip = params[:zip] if(params[:zip] != nil and params[:zip] != "")
+    @new_Shop.adress = params[:adress] if(params[:adress] != nil and params[:adress] != "")
+    @new_Shop.latitude = params[:latitude].to_f if(params[:latitude] != nil and params[:latitude] != "")
+    @new_Shop.longitude = params[:longitude].to_f if(params[:longitude] != nil and params[:longitude] != "")
+    @new_Shop.city = params[:city] if(params[:city] != nil and params[:city] != "")
+    @new_Shop.phone = params[:phone] if(params[:phone] != nil and params[:phone] != "")
+    @new_Shop.country_code = params[:country_code] if(params[:country_code] != nil and params[:country_code] != "")
+
+    if @new_Shop.save() then redirect_to ({:action => "index"})
+    else redirect_to ({:action => "new"})
+    end
   end
 
   def delete
+    @cible = Shop.find_by_id(params[:id])
+    @cible.destroy
+    puts "done"
+    redirect_to ({:action => "index"})
   end
 
   def edit
     @target = Shop.find_by_id(params[:id])
+  end
+  def update
+    @update_target = Shop.find(params[:id])
+    @update_target.chain = params[:chain] if(params[:chain] != nil and params[:chain] != "")
+    @update_target.name = params[:name] if(params[:name] != nil and params[:name] != "")
+    @update_target.zip = params[:zip] if(params[:zip] != nil and params[:zip] != "")
+    @update_target.adress = params[:adress] if(params[:adress] != nil and params[:adress] != "")
+    @update_target.latitude = params[:latitude].to_f if(params[:latitude] != nil and params[:latitude] != "")
+    @update_target.longitude = params[:longitude].to_f if(params[:longitude] != nil and params[:longitude] != "")
+    @update_target.city = params[:city] if(params[:city] != nil and params[:city] != "")
+    @update_target.phone = params[:phone] if(params[:phone] != nil and params[:phone] != "")
+    @update_target.country_code = params[:country_code] if(params[:country_code] != nil and params[:country_code] != "")
 
+    @update_target.save()
+    redirect_to ({:action => "index"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ Rails.application.routes.draw do
 
   get 'positions_controller/edit'
 
+  post 'positions_controller/update'
+
+  post 'positions_controller/add'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
